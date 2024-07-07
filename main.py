99e15728985da5ac102f96099b3df4908e0adacd
rohanp29 <168647197+rohanp29@users.noreply.github.com>
2024-07-07 19:52:55 -0400

    Updated .gitignore
    Updated .gitignore so that we exclude large datasets for microlearning content as well as .env for API Key Protection

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 #This is the main script for the hackathon project
-print("Hello MediHacks!")
+#print("Hello MediHacks!")
+
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+def main():
+
+    primekg = pd.read_csv('disease_features.csv', low_memory = False)
+    print(primekg.head(50))
+
+if __name__ == "__main__":
+    main()

93b2d12b487249d9bc7393dc43fbaa1e9cbe049c
rohanp29 <168647197+rohanp29@users.noreply.github.com>
2024-07-10 17:39:03 -0400

    Starting XCode Project
    XCode Project Created in Folder within Purkinje

---
+++ Xcode/SkillDrop/SkillDrop/SkillDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillDropApp.swift
+//  SkillDrop
+//
+//  Created by Devin Weikert on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SkillDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

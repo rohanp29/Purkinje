d4da80cf93a837ce0904e3506b607fd95fbd24c2
rohanp29 <168647197+rohanp29@users.noreply.github.com>
2024-07-11 21:57:03 -0400

    State Before Database Created
    Just in case we need to roll back

--- Xcode/SkillDrop/SkillDrop/ContentView.swift
+++ Xcode/SkillDrop/SkillDrop/ContentView.swift
@@ -14,86 +14,92 @@ struct ContentView: View {
     @State private var userIsLoggedIn = false
     
     var body: some View {
-        VStack {
-            ZStack {
-                Color.black
-                RoundedRectangle(cornerRadius: 30.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
-                    .foregroundStyle(.linearGradient(colors: [.blue, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width:1000, height: 400)
-                    .rotationEffect(.degrees(135))
-                    .offset(y:-350)
+        if userIsLoggedIn {
+            ListView()
+        } else {
+            content
+        }
+        //.padding()
+    }
+    
+    var content: some View {
+        ZStack {
+            Color.black
+            RoundedRectangle(cornerRadius: 30.0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.linearGradient(colors: [.blue, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width:1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y:-350)
+            
+            VStack(spacing:20){
+                Text("Skill Drop")
+                    .foregroundColor(.white)
+                    .font(.system(size:40,weight:.bold, design: .rounded))
+                    .offset(x:-100, y:-100)
                 
-                VStack(spacing:20){
-                    Text("Skill Drop")
-                        .foregroundColor(.white)
-                        .font(.system(size:40,weight:.bold, design: .rounded))
-                        .offset(x:-100, y:-100)
-                    
-                    TextField("Email", text:$email)
-                        .foregroundColor(.white)
-                        .textFieldStyle(.plain)
-                        .placeholder(when: email.isEmpty) {
-                            Text("Email")
-                                .foregroundColor(.white)
-                                .bold()
-                        }
-                    
-                    Rectangle()
-                        .frame(width: 350, height: 1)
-                        .foregroundColor(.white)
-                    
-                    SecureField("Password", text: $password)
-                        .foregroundColor(.white)
-                        .textFieldStyle(.plain)
-                        .placeholder(when: password.isEmpty) {
-                            Text("Password")
-                                .foregroundColor(.white)
-                                .bold()
-                        }
-                    
-                    Rectangle()
-                        .frame(width: 350, height: 1)
-                        .foregroundColor(.white)
-                    
-                    Button {
-                        register()
-                    } label: {
-                        Text("Sign up")
-                            .bold()
-                            .frame(width: 200, height: 40)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    .fill(.linearGradient(colors: [.blue], startPoint: .topLeading, endPoint: .bottomTrailing))
-                            )
+                TextField("Email", text:$email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
                             .foregroundColor(.white)
-                    }
-                    .padding(.top)
-                    .offset(y:100)
-                    
-                    Button{
-                        login()
-                    }label:{
-                        Text("Already have an account? Login")
                             .bold()
-                            .foregroundColor(.white)
-                    
                     }
-
-                    .padding(.top)
-                    .offset(y:110)
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    register()
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.blue], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.white)
                 }
-                .frame(width:350)
-                .onAppear {
-                    Auth.auth().addStateDidChangeListener { auth, user in
-                        if user != nil {
-                            userIsLoggedIn.toggle()
-                        }
+                .padding(.top)
+                .offset(y:100)
+                
+                Button{
+                    login()
+                }label:{
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                
+                }
+
+                .padding(.top)
+                .offset(y:110)
+            }
+            .frame(width:350)
+            .onAppear {
+                Auth.auth().addStateDidChangeListener { auth, user in
+                    if user != nil {
+                        userIsLoggedIn.toggle()
                     }
                 }
             }
-            .ignoresSafeArea()
         }
-        //.padding()
+        .ignoresSafeArea()
     }
     
     func login() {
---
+++ Xcode/SkillDrop/SkillDrop/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  SkillDrop
+//
+//  Created by Rohan and Rebecca on 7/11/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
